17d0b5d4172a226914cfe3fd4c93e40f755efdb7
Luca De Feo <dfl@deagol>
2013-05-28 03:37:23 +0200

    Initial development based on Bootstrap

---
+++ js/main.js
@@ -0,0 +1,5 @@
+(function() {
+    var to = 'mai' + 'lto' + String.fromCharCode(40+18);
+    var m = 'luca.de-feo' + String.fromCharCode(8*8) + 'uvsq.fr';
+    $('#mungemail').html('<a href="' + to + m + '">' + m + '</a>');
+})();
