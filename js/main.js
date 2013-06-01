a8d2559f9fce93d05a72c2ae2cd40773c0e86ad7
Luca De Feo <dfl@deagol>
2013-06-01 22:19:17 +0200

    Refactoring, layout, searchbox
    - Refactored two layouts into one
    - Minor layout modifications (menu, footer)
    - Added simple google serach box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+/* E-mail address de-munger */
 (function() {
     var to = 'mai' + 'lto' + String.fromCharCode(40+18);
     var m = 'luca.de-feo' + String.fromCharCode(8*8) + 'uvsq.fr';
     $('#mungemail').html('<a href="' + to + m + '">' + m + '</a>');
+})();
+
+/* Simple google search box */
+(function() {
+    $('#search-glass').click(function() {
+	document.location.href= 'https://www.google.com/search?q=' + encodeURI([$('#search-field').val(), 'site:defeo.lu'].join('+'));
+    });
 })();
