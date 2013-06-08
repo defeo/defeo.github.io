d98618ee244c55de3d866a65520e4d32f2a86472
Luca De Feo <dfl@deagol>
2013-06-09 00:29:35 +0200

    Improved keyboard shortcuts

--- js/main.js
+++ js/main.js
@@ -63,33 +63,110 @@ $(function() {
 
     /* Keyboard navigation */
     if (!touch) {
-	var keys = {};
-	$('[data-toggle="key-nav"]').each(function() {
-	    var $this = $(this);
-	    var k = $this.text().toUpperCase()[0];
-	    keys[k.charCodeAt(0)] =  $this.attr('data-hash');
-	    $('#key-help .modal-body dl').append('<dt>' + k + '</dt>' + '<dd>' +
-						 $this.attr('title') + '</dd>');
+	/* Add hotkeys from the document */
+	var modal = $('<table class="hotkey-list"></table>').appendTo('#hotkey-help .modal-body');
+	$('[data-toggle="hotkey"]').hotkey('on', function(k) {
+	    this.addClass('hotkey');
+	    var row = $('<tr>').appendTo(modal);
+	    k && row.append('<td><code>'
+			    + k.toLowerCase().split(' ').join('</code><code>')
+			    + '</code></td>');
+
+	    var v = this.attr('title');
+	    v && row.append('<td>' + v + '</td>');
 	});
-	var meta = [16, 17, 18, 91, 225]
-	
-	var metas = 0;
-	$(document).on('keydown keyup', function(e) {
-	    if (!metas && e.type == 'keydown') {
-		if (e.which in keys)
-		    window.location.hash = '#' + keys[e.which];
-		else if (e.which == 36)     // Home key
-		    window.location.hash = '#';
-		else if (e.which == 191) {  // / key
-		    console.log('?');
-		    $('#key-help').modal('toggle');
-		    e.preventDefault();
-		} else if (meta.indexOf(e.which) >= 0) {
-		    metas++;
-		}
-	    } else if (metas && e.type == 'keyup' && meta.indexOf(e.which) >= 0) {
-		metas--;
-	    }
+	// Event for ESC key inside forms
+	$('input').on('hotkey.blur', function() {
+	    $(this).blur();
+	});
+	// Event for help modal
+	$(document).on('hotkey.help', function() {
+	    $('#hotkey-help').modal('toggle');
 	});
     }
 });
+
+
+/**
+  Hotkeys!
+
+  This jQuery plugin makes it easy to add hotkeys to the document.
+*/
+(function($) {
+    var data = {
+	'ns'      : '.hotkeys',
+	'on'      : 'hotkeys',
+	'key'     : 'data-hotkey',
+	'special' : 'data-hotkey-special',
+	'selector': 'data-hotkey-selector',
+	'href'    : 'href',
+	'focus'   : 'data-hotkey-focus',
+	'trigger' : 'data-hotkey-trigger',
+    };
+    
+    function hotkeys_on(callback) {
+	var event, k =
+	    ((event = 'keypress') && this.attr(data.key)) || 
+	    ((event = 'keydown') && this.attr(data.special)) || 
+	    ((event = 'keypress') && this.text()[0]);
+
+	var selector = this.attr(data.selector) || document;
+
+	if (k) {
+	    $(selector).on(event + data.ns, null, k, (function(href, focus, trigger) {
+		return function(e) {
+		    if (href !== undefined) {
+			window.location.href = href;
+		    }
+		    if (focus !== undefined) {
+			$(focus).focus();
+		    }
+		    if (trigger !== undefined) {
+			$(this).trigger(trigger);
+		    }
+		    e.preventDefault();
+		};
+	    })(this.attr(data.href),
+	       this.attr(data.focus),
+	       this.attr(data.trigger)));
+	}
+
+	// Notify the callback that we have added a new hotkey
+	if (callback)
+	    callback.call(this, k);
+    }
+
+    function hotkeys_off(callback) {
+	var selector = this.attr(data.selector) || document;
+	$(selector).off('keypress' + data.ns + ' keydown' + data.ns);
+	if (callback)
+	    callback.call(this, k);
+    }
+
+    jQuery.fn.extend({
+	hotkey: function(action, callback) {
+	    if (action == 'on')
+		return this.each(function() {
+		    hotkeys_on.call($(this), callback);
+		    $(this).data(data.on, true)
+		});
+	    else if (action == 'off')
+		return this.each(function() {
+		    hotkeys_off.call($(this), callback);
+		    $(this).data(data.on, null)
+		});
+	    else if (action == 'toggle') 
+		return this.each(function() {
+		    var $this = $(this);
+		    if ($this.data(data.on)) {
+			hotkeys_on.call($this, callback);
+			$this.data(data.on, true);
+		    } else {
+			hotkeys_off.call($this, callback);
+			$this.data(data.on, null);
+		    }
+		});
+	}
+    });
+})(jQuery);
+
