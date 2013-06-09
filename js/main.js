6fdc90e924e0f61b58244ce20fd3ae092594a341
Luca De Feo <dfl@deagol>
2013-06-09 17:46:11 +0200

    Improved hotkeys, added toggle button

--- js/main.js
+++ js/main.js
@@ -62,27 +62,49 @@ $(function() {
     });
 
     /* Keyboard navigation */
-    if (!touch) {
-	/* Add hotkeys from the document */
-	var modal = $('<table class="hotkey-list"></table>').appendTo('#hotkey-help .modal-body');
-	$('[data-toggle="hotkey"]').hotkey('on', function(k) {
-	    this.addClass('hotkey');
-	    var row = $('<tr>').appendTo(modal);
-	    k && row.append('<td><code>'
-			    + k.toLowerCase().split(' ').join('</code><code>')
-			    + '</code></td>');
+    $('#hotkeys-btn').click(function() {
+	// Deactivate button while we enable
+	$(this).prop('disabled', true);
+	// Only fill the help modal if this is the first time
+	var help = $('#hotkey-help .modal-body');
+	var modal;
+	if (!help.data('hotkey-filled')) {
+	    modal = $('<table class="hotkey-list"></table>').appendTo(help);
+	}
+	// Toggle hotkeys
+	$('[data-toggle="hotkey"]').hotkey('toggle', function(k) {
+	    this.toggleClass('hotkey');
+	    if (modal) {
+		var row = $('<tr>').appendTo(modal);
+		k && row.append('<td><code>'
+				+ k.toLowerCase().split(' ').join('</code><code>')
+				+ '</code></td>');
 
-	    var v = this.attr('title');
-	    v && row.append('<td>' + v + '</td>');
+		var v = this.attr('title');
+		v && row.append('<td>' + v + '</td>');
+	    }
 	});
-	// Event for ESC key inside forms
-	$('input').on('hotkey.blur', function() {
-	    $(this).blur();
-	});
-	// Event for help modal
-	$(document).on('hotkey.help', function() {
-	    $('#hotkey-help').modal('toggle');
-	});
+	// Notify the help modal is filled
+	// (works because .hotkey() uses no asynchronous code)
+	help.data('hotkey-filled', true);
+	// Prepare button
+	$(this).toggleClass('active');
+	$(this).removeProp('disabled');
+    })
+    // Event for ESC key inside forms
+    $('input').on('hotkey.blur', function() {
+	$(this).blur();
+    });
+    // Event for help modal
+    $(document).on('hotkey.help', function() {
+	$('#hotkey-help')
+	    .modal('toggle')
+	    .css('top', $(document).scrollTop() + 50 + 'px');
+    });
+
+    /* If not on a touch device, activate hotkeys */
+    if (!touch) {
+	$('#hotkeys-btn').click();
     }
 });
 
@@ -104,7 +126,7 @@ $(function() {
 	'trigger' : 'data-hotkey-trigger',
     };
     
-    function hotkeys_on(callback) {
+    function hotkeys(callback, on) {
 	var event, k =
 	    ((event = 'keypress') && this.attr(data.key)) || 
 	    ((event = 'keydown') && this.attr(data.special)) || 
@@ -113,32 +135,29 @@ $(function() {
 	var selector = this.attr(data.selector) || document;
 
 	if (k) {
-	    $(selector).on(event + data.ns, null, k, (function(href, focus, trigger) {
-		return function(e) {
-		    if (href !== undefined) {
-			window.location.href = href;
-		    }
-		    if (focus !== undefined) {
-			$(focus).focus();
-		    }
-		    if (trigger !== undefined) {
-			$(this).trigger(trigger);
-		    }
-		    e.preventDefault();
-		};
-	    })(this.attr(data.href),
-	       this.attr(data.focus),
-	       this.attr(data.trigger)));
+	    if (on) {
+		$(selector).on(event + data.ns, null, k, (function(href, focus, trigger) {
+		    return function(e) {
+			if (href !== undefined) {
+			    window.location.href = href;
+			}
+			if (focus !== undefined) {
+			    $(focus).focus();
+			}
+			if (trigger !== undefined) {
+			    $(this).trigger(trigger);
+			}
+			e.preventDefault();
+		    };
+		})(this.attr(data.href),
+		   this.attr(data.focus),
+		   this.attr(data.trigger)));
+	    } else {
+		$(selector).off('keypress' + data.ns + ' keydown' + data.ns);
+	    }
 	}
 
-	// Notify the callback that we have added a new hotkey
-	if (callback)
-	    callback.call(this, k);
-    }
-
-    function hotkeys_off(callback) {
-	var selector = this.attr(data.selector) || document;
-	$(selector).off('keypress' + data.ns + ' keydown' + data.ns);
+	// Notify the callback that we have toggled the hotkey
 	if (callback)
 	    callback.call(this, k);
     }
@@ -147,24 +166,19 @@ $(function() {
 	hotkey: function(action, callback) {
 	    if (action == 'on')
 		return this.each(function() {
-		    hotkeys_on.call($(this), callback);
+		    hotkeys.call($(this), callback, true);
 		    $(this).data(data.on, true)
 		});
 	    else if (action == 'off')
 		return this.each(function() {
-		    hotkeys_off.call($(this), callback);
+		    hotkeys.call($(this), false);
 		    $(this).data(data.on, null)
 		});
 	    else if (action == 'toggle') 
 		return this.each(function() {
-		    var $this = $(this);
-		    if ($this.data(data.on)) {
-			hotkeys_on.call($this, callback);
-			$this.data(data.on, true);
-		    } else {
-			hotkeys_off.call($this, callback);
-			$this.data(data.on, null);
-		    }
+		    var on = $(this).data(data.on);
+		    hotkeys.call($(this), callback, !on);
+		    $(this).data(data.on, !on);
 		});
 	}
     });
